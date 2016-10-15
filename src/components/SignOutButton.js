fa4d1ba65bcebdf993c3107807cdeafc2b1b3900
Piotr Staniów <staniowp@gmail.com>
2016-10-15 18:55:44 +0200

    Handle sign out failure

--- src/components/SignOutButton.js
+++ src/components/SignOutButton.js
@@ -3,16 +3,25 @@ import { Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { signOut } from '../actions/auth.actions';
 import { fetchLogout } from '../api/connectors';
+import { raiseError } from '../actions/error.actions';
 
 const mapDispatchToProps = dispatch => ({
-    signOut: (response) => response.status === 200 ? dispatch(signOut()) : () => {},
+    dispatchSignOut: () => dispatch(signOut()),
+    raiseFailure: () => dispatch(raiseError('Failed to sign out properly.')),
 });
 
 @connect(null, mapDispatchToProps)
 export default class SignInButton extends Component {
     signOut = () => {
         fetchLogout()
-            .then(this.props.signOut);
+            .then(this.onSignOutDone);
+    };
+
+    onSignOutDone = (response) => {
+        if (response.status === 200) {
+            return this.props.dispatchSignOut();
+        }
+        return this.props.raiseFailure();
     };
 
     render() {
