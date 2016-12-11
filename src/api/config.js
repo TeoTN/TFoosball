0926e74d92c20b7c49d34cd667d4af56001210c4
Piotr Staniów <staniowp@gmail.com>
2016-12-11 16:08:26 +0100

    Prepare for deployment (#7)
    * Configure OAuth2 endpoint
    * Update dependencies
    * Get API server root from env var
    * Configurable OAuth client id
    * Update config/env vars names.

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,5 @@
-export const URL_ROOT = 'http://localhost:8000/';
-export const API_ROOT = `${URL_ROOT}api/`;
-export const AUTH_REDIR_URL = `${URL_ROOT}auth/callback`;
+export const API_SERVER = process.env.REACT_APP_API_SERVER || 'http://localhost:8000/';
+export const API_ROOT = `${API_SERVER}api/`;
+export const AUTH_REDIR_URL = `${API_SERVER}auth/callback`;
+export const OAUTH_CLIENT_ID = process.env.REACT_APP_OAUTH_CLIENT_ID ||
+    '907377379670-suvgso3siks409qfqgmqvfk2c18g4buh.apps.googleusercontent.com';
