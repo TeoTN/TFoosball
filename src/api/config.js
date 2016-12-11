8969f509b226284af26e499f283b07a72bf5e1fb
Piotr Staniów <staniowp@gmail.com>
2016-12-11 15:46:04 +0100

    Debug: expose env vars to window

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,4 @@
+window.env = process.env;
 export const URL_ROOT = process.env.API_ROOT || 'http://localhost:8000/';
 export const API_ROOT = `${URL_ROOT}api/`;
 export const AUTH_REDIR_URL = `${URL_ROOT}auth/callback`;
