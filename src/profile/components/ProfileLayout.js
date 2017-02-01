bf0f229ed8bfd1a5177117fc6b49d09234f79d17
Piotr Staniów <staniowp@gmail.com>
2017-02-01 13:11:48 +0100

    Support Gravatar photos

--- src/profile/components/ProfileLayout.js
+++ src/profile/components/ProfileLayout.js
@@ -5,6 +5,7 @@ import ProfileStats from './ProfileStats';
 import { Panel, NavItem, Nav } from 'react-bootstrap';
 import { withRouter } from 'react-router';
 import { LinkContainer } from 'react-router-bootstrap';
+import md5 from 'md5';
 
 const mapStateToProps = ({profile}) => ({profile});
 
@@ -13,9 +14,17 @@ const mapStateToProps = ({profile}) => ({profile});
 export default class ProfileLayout extends Component {
     render() {
         const { children, profile, params: {username}} = this.props;
+        const avatarURL = !profile.email ? '' : `https://www.gravatar.com/avatar/${md5(profile.email )}`;
+
         return (
             <div>
-                <h1>Profile &nbsp;<small>{ username } ({ profile.exp } XP)</small></h1>
+                <h1>
+
+                    <picture className="profile-photo">
+                        <img src={avatarURL} type="image/jpeg" alt="avatar" />
+                    </picture>
+                    { username } <small>{ profile.exp } XP</small>
+                </h1>
                 <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
                     <LinkContainer to={{ pathname: `/profile/${username}/stats`}}>
                         <NavItem eventKey="1" href="#">Statistics</NavItem>
