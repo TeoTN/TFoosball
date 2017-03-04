1e7dbbebc67d724aaf9a2f5d9e8ea8081c8d5530
Piotr Staniów <staniowp@gmail.com>
2017-03-04 18:22:37 +0100

    Unit tests for users sagas

--- src/test/users.sagas.test.js
+++ src/test/users.sagas.test.js
@@ -3,41 +3,115 @@ import api from '../api';
 import response from '../assets/mocks/users.json';
 import * as UserActions from '../users/user.actions';
 import { raiseError } from '../shared/notifier.actions';
-import { fetchUsers } from '../users/users.sagas.js';
+import { fetchUsers, fetchUpdateUsers } from '../users/users.sagas.js';
+import { getCurrentTeam } from '../shared/teams/teams.sagas';
 
-describe('Fetch user list - success scenario', () => {
+describe('FetchUsers saga ', () => {
     const iterator = fetchUsers();
-    const currentTeamId = 1;
+    const currentTeam = {id: 1};
+    const url = api.urls.teamMemberList(currentTeam.id);
 
-    it('should select team id', () => {
-        expect(JSON.stringify(iterator.next().value)).toEqual(JSON.stringify(select(() => currentTeamId)));
+    describe('Scenario 1 - success scenario', () => {
+        it('should select team id', () => {
+            const iter = iterator.next().value;
+            expect(iter).toEqual(call(getCurrentTeam));
+        });
+
+        it('should call fetch api', () => {
+            expect(iterator.next(currentTeam).value).toEqual(call(api.requests.get, url));
+        });
+
+        it('should put response action', () => {
+            const iter = iterator.next(response).value;
+            expect(iter).toEqual(put(UserActions.receiveUsers(response)));
+        });
+
+        it('should return from saga', () => {
+            const iter = iterator.next().done;
+            expect(iter).toBe(true);
+        });
     });
 
-    it('should call fetch api', () => {
-        const url = api.urls.teamMemberList(currentTeamId);
-        expect(iterator.next(currentTeamId).value).toEqual(call(api.requests.get, url));
+
+    describe('Scenario 2: - failure scenario', () => {
+        const iterator = fetchUsers();
+        const errorMsg = 'Unable to fetch user list';
+
+        it('should select team id', () => {
+            const iter = iterator.next().value;
+            expect(iter).toEqual(call(getCurrentTeam));
+        });
+
+        it('should call fetch api', () => {
+            const iter = iterator.next(currentTeam).value;
+            expect(iter).toEqual(call(api.requests.get, url));
+        });
+
+        it('should put raise error action', () => {
+            const iter = iterator.throw(errorMsg).value;
+            expect(iter).toEqual(put(raiseError(errorMsg)));
+        });
+
+        it('should return from saga', () => {
+            const iter = iterator.next().done;
+            expect(iter).toBe(true);
+        });
     });
 
-    it('should put response action', () => {
-        expect(iterator.next(response).value).toEqual(put(UserActions.receiveUsers(response)));
-    });
 });
 
-describe('Fetch user list - failure scenario', () => {
-    const iterator = fetchUsers();
-    const errorMsg = 'Unable to fetch user list';
-    const currentTeamId = 1;
 
-    it('should select team id', () => {
-        expect(JSON.stringify(iterator.next().value)).toEqual(JSON.stringify(select(() => currentTeamId)));
+describe('FetchUpdateUsers saga', () => {
+    const currentTeam = { id: 6 };
+    const url = api.urls.teamMemberList(currentTeam.id);
+    const response = [{id: 6}, {id: 7}];
+    const errorMsg = 'Failed to fetch users list';
+
+    describe('Scenario 1: Success', () => {
+        const iterator = fetchUpdateUsers();
+
+        it('should call to get current team', () => {
+            const iter = iterator.next().value;
+            expect(iter).toEqual(call(getCurrentTeam))
+        });
+
+        it('should call API to get team users list', () => {
+            const iter = iterator.next(currentTeam).value;
+            expect(iter).toEqual(call(api.requests.get, url, {}, errorMsg));
+        });
+
+        it('should put updateUsers action', () => {
+            const iter = iterator.next(response).value;
+            expect(iter).toEqual(put(UserActions.updateUsers(response)));
+        });
+
+        it('should return from saga', () => {
+            const iter = iterator.next(response).done;
+            expect(iter).toBe(true);
+        });
     });
 
-    it('should call fetch api', () => {
-        const url = api.urls.teamMemberList(currentTeamId);
-        expect(iterator.next(currentTeamId).value).toEqual(call(api.requests.get, url));
-    });
+    describe('Scenario 2: Failure', () => {
+        const iterator = fetchUpdateUsers();
 
-    it('should put raise error action', () => {
-        expect(iterator.throw(errorMsg).value).toEqual(put(raiseError(errorMsg)));
+        it('should call to get current team', () => {
+            const iter = iterator.next().value;
+            expect(iter).toEqual(call(getCurrentTeam))
+        });
+
+        it('should call API to get team users list', () => {
+            const iter = iterator.next(currentTeam).value;
+            expect(iter).toEqual(call(api.requests.get, url, {}, errorMsg));
+        });
+
+        it('should put updateUsers action', () => {
+            const iter = iterator.throw(errorMsg).value;
+            expect(iter).toEqual(put(raiseError(errorMsg)));
+        });
+
+        it('should return from saga', () => {
+            const iter = iterator.next(response).done;
+            expect(iter).toBe(true);
+        });
     });
 });
