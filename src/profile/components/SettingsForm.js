12f9fc271bad9067076855cccc4b5fbf647f22f7
Piotr Staniów <staniowp@gmail.com>
2017-04-04 23:07:34 +0200

    Add validation to profile settings

--- src/profile/components/SettingsForm.js
+++ src/profile/components/SettingsForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Form, FormGroup, Row, Col, Button } from 'react-bootstrap';
 import { reduxForm, Field } from 'redux-form';
 import SettingsInput from './SettingsInput';
+import { isUsername, isName } from '../../validators';
 
 
 const SettingsForm = ({ handleSubmit, pristine }) => {
@@ -10,10 +11,9 @@ const SettingsForm = ({ handleSubmit, pristine }) => {
             <Col xs={12} sm={8}>
                 <h4>Profile settings</h4>
                 <Form onSubmit={handleSubmit} horizontal>
-                    <Field component={SettingsInput} label="Username" name="username"/>
-                    <Field component={SettingsInput} label="First name" name="first_name" />
-                    <Field component={SettingsInput} label="Last name" name="last_name" />
-
+                    <Field component={SettingsInput} label="Username" name="username" validate={isUsername} />
+                    <Field component={SettingsInput} label="First name" name="first_name" validate={isName} />
+                    <Field component={SettingsInput} label="Last name" name="last_name" validate={isName} />
                     <FormGroup>
                         <Col smOffset={3} sm={8}>
                             <Button type="submit" bsStyle={pristine ? 'default' : 'success'} block disabled={pristine}>
--- src/profile/components/SettingsInput.js
+++ src/profile/components/SettingsInput.js
@@ -5,13 +5,15 @@ import { ControlLabel, FormControl, FormGroup, Col } from 'react-bootstrap';
 export default class SettingsInput extends React.Component {
 
     render () {
-        const { placeholder, type, input, meta, label } = this.props;
+        const { placeholder, type, input, meta: {error, touched, warning}, label } = this.props;
 
         return (
-            <FormGroup controlId={input.name} validationState={meta.error ? 'error' : null}>
+            <FormGroup controlId={input.name} validationState={ error ? 'error' : null }>
                 <Col componentClass={ControlLabel} sm={3}>{label}:</Col>
                 <Col sm={8}>
-                <FormControl type={type} placeholder={placeholder} value={input.value} onChange={input.onChange} />
+                    <FormControl type={type} placeholder={placeholder} value={input.value} onChange={input.onChange} />
+                    { error && <span className="text-danger h6">{error.message}</span>}
+
                 </Col>
                 {/*<FormControl.Feedback />*/}
             </FormGroup>
